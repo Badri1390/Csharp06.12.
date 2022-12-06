fd7708c98274014478cfd2673aeddae880f949eb
Badri <9299547077@mail.ru>
2022-12-06 22:40:58 +0300

    Палиндром или нет

---
+++ dz19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+if(num.ToString()[0] == num.ToString()[4] && num.ToString()[1] == num.ToString()[3]){
+    System.Console.WriteLine("Да");
+}
+else{
+    System.Console.WriteLine("нет");
+}
